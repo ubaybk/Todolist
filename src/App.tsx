0e06feb3ee608b5a1d7be1bc5f38bee44371bb8c
ubaybk <bayu05061992@gmail.com>
2024-03-01 08:26:37 +0700

    menambakan fungsi add

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,32 @@ import Todo from "./components/Todo"
 
 
 function App() {
-  const [todo] = useState(['Code', 'Code2'])
-
+  const [todo,setTodo] = useState<any>([])
+  const [inputTodo,setInputTodo] = useState("")
+  const addTodo = () =>{
+    if (inputTodo){
+      setTodo([...todo,inputTodo])
+      setInputTodo("")
+    }else{
+      alert("input text")
+    }
+    
+  }
+  console.log(inputTodo)
+  // console.log(todo)
   return(
     <div className="bg-teal-300 h-screen w-screen p-4">
       <div className="bg-slate-100 p-4 max-w-[500px] w-full m-auto rounded-lg shadow-xl ">
         <h1 className="text-center mb-4 text-2xl font-bold text-teal-700">TODOLIST</h1>
-        <Form/>
+        {/* <Form/> */}
+        <input className='w-full text-xl rounded-lg p-[3px]' onChange={(e)=>setInputTodo(e.target.value)}value={inputTodo} />
+        <button onClick={()=>addTodo()}>submit</button>
         <ul>
-          {todo.map((todos, index) =>(
+          {todo.map((todos:any, index:any) =>(
             <Todo key={index} todos = {todos}/>
           ))}
         </ul>
+        <p className="text-center">Kamu memiliki {todo.length} list</p>
       </div>
     </div>
   )
