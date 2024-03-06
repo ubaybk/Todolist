c2a69ac7ff3f62142b0a105ce087776cba9940b3
Bayu Kurniawan <bayu05061992@gmail.com>
2024-03-06 11:03:31 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: "/Todolist/",
-  build: {
-    rollupOptions: {
-      external: "src/App.css",
-    },
-  },
+ 
+ 
 });
